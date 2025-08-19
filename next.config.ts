d157c131dcd6f6e0a54d14dd659d587933336304
lymbf <paw.czekaj@gmail.com>
2025-08-19 15:49:36 +0200

    disabled eslint errors

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
             bodySizeLimit: '3mb',
         },
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    }
 };
 
 export default nextConfig;
